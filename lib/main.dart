f7019cad88b5ee75d7f0560d6fb6bbb5fbe9db80
Wataru <wataruoshima@my.smccd.edu>
2022-04-28 09:19:52 -0700

    updated podfile so that we can launch this app in iOS

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ import 'package:split/Screens/Home/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 
 }
