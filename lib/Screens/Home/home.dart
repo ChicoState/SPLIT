c833ec18681d18dc66521a040ef50928f9635527
Wataru <wataruoshima@my.smccd.edu>
2022-05-08 17:56:37 -0700

    added sample calender

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -10,7 +10,7 @@ import 'package:split/Services/database.dart';
 import 'package:split/Services/database.dart';
 import 'package:split/Screens/Home/users_list.dart';
 import 'package:split/Models/appUser.dart';
-
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 import '../../Models/appUser.dart';
 
 final FirebaseAuth auth = FirebaseAuth.instance;
@@ -309,6 +309,11 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeState();
 }
 
+
+
+
+
+
 class _HomeState extends State<HomeScreen> {
 
   final valueHolder = TextEditingController();
@@ -346,8 +351,20 @@ class _HomeState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget> [
-            Text("Placeholder Calendar"),
-            //placeholder calendar.  Should replace with google calendar
+                SizedBox(
+                  height: 500,
+                  width: 500,
+                  child: SfCalendar(
+                    view: CalendarView.month,
+                    initialSelectedDate: DateTime.now(),
+                    dataSource: MeetingDataSource(_getDataSource()),
+                    monthViewSettings: MonthViewSettings(
+                        appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+
+                  ),
+                ),
+
+                    //placeholder calendar.  Should replace with google calendar
 
             // TableCalendar(
             //   firstDay: DateTime.utc(2010, 10, 16),
@@ -390,6 +407,67 @@ class _HomeState extends State<HomeScreen> {
     );
   }
 }
+
+// calender sample --------------------------
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+}
+
+List<Meeting> _getDataSource() {
+  final List<Meeting> meetings = <Meeting>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+  DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+  meetings.add(Meeting(
+      'Payment', startTime, endTime, const Color(0xFF0F8644), false));
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+//----------------------------- calender sample----------------------------
+
+
+
+
+
+
 
 class GroupList extends StatelessWidget{
 
